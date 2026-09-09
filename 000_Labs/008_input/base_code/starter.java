bc75ea0d91df114bd2afbc9bcb4ba300cad2049b
angelinelp00-tech <angelinelp00@gmail.com>
2026-09-09 19:07:46 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,21 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter your name here: ");
+		String name = sc.nextLine();
+		System.out.print("Enter your age (one whole number): ");
+		int age = sc.nextLine();
+		System.out.print("Enter your birth month (one whole number): ");
+		int month = sc.nextLine();
+		System.out.print("Enter your birth date (one whole number): ");
+		int day = sc.nextLine();
+		System.out.print("Enter your birth year (one whole number): ");
+		int year = sc.nextLine();
+		System.out.print("How much is a buck fifty?: ");
+		String buck = sc.nextLine();
+		
+		System.out.print("Hello " + name + "")
 	}
 }
