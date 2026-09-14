efe0621aadc7406e4258662daa44d3308d30340f
angelinelp00-tech <angelinelp00@gmail.com>
2026-09-14 19:39:26 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Angeline Peng
+ *  Date: 9/9/26
 */
 
 import java.util.Scanner;
@@ -11,19 +11,20 @@ class starter {
 		// the command window when you compile and run this program.
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Please press enter to confirm your answer on the following questions:");
 		System.out.print("Enter your name here: ");
 		String name = sc.nextLine();
 		System.out.print("Enter your age (one whole number): ");
-		int age = sc.nextLine();
+		int age = sc.nextInt();
 		System.out.print("Enter your birth month (one whole number): ");
-		int month = sc.nextLine();
+		int month = sc.nextInt();
 		System.out.print("Enter your birth date (one whole number): ");
-		int day = sc.nextLine();
+		int day = sc.nextInt();
 		System.out.print("Enter your birth year (one whole number): ");
-		int year = sc.nextLine();
-		System.out.print("How much is a buck fifty?: ");
-		String buck = sc.nextLine();
+		int year = sc.nextInt();
+		System.out.print("How much is a buck fifty? (Enter as a whole or decimal number): $");
+		double buck = sc.nextDouble();
 		
-		System.out.print("Hello " + name + "")
+		System.out.println("Hello " + name + ", you're " + age + " years old, your birthday is " + month + "/" + day + "/" + year + ", and you believe that a buck fifty is $" + buck);
 	}
 }
--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Angeline Peng
+ *  Date: 9/10/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,26 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Here are the answers:");
+		System.out.println(Math.max(13 - 6 * 11, 30 % 7 * (-2)));
+		System.out.println(Math.sqrt(3 * 8 + 31 % 7));
+		System.out.println(Math.pow(37 / 3, 35 % 21));
+		double pow2 = Math.pow(2, 14 % 3);
+		double sqrt2 = Math.sqrt(2*6);
+		System.out.println(Math.max(pow2, sqrt2));
+		System.out.println("");
+
+		System.out.println("Please press the ENTER button to confirm your answer.");
+		System.out.print("Enter a whole number: ");
+		int x = sc.nextInt();
+		System.out.print("Enter another whole number: ");
+		int y = sc.nextInt();
+
+		int max = Math.max(x,y);
+		double sqrt = Math.sqrt(y);
+		double pow = Math.pow(x,y);
+		System.out.println("The max of " + x + " and " + y + " is " + max + ", the square root of " + y + " is " + sqrt + ", and the value of " + x + "^" + y + " is " + pow);
 	}
 }
